e2b0460159e05dfefb33df3c95599e6acbcd7668
Adarsh M <adarshm954@gmail.com>
2019-01-26 20:25:43 +0530

    updated

--- admin/login.php
+++ admin/login.php
@@ -3,13 +3,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Sign In</title>
+	<title>Admin</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
 	<div class="header">
-		<h2>Login</h2>
+		<h2>Admin Login</h2>
 	</div>
 	
 	<form method="post" action="login.php">
@@ -27,9 +27,6 @@
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_user">Login</button>
 		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
 	</form>
 
 
--- template/change.php
+++ template/change.php
@@ -1,144 +1,99 @@
+<?php
+session_start();
+
+$a=$_SESSION['username'];
+
+if($a=="")
+{
+header('location: .././login');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Admin</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href=".././vendors/iconfonts/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href=".././vendors/css/vendor.bundle.base.css">
-  <link rel="stylesheet" href=".././vendors/css/vendor.bundle.addons.css">
-  <!-- endinject -->
-  <!-- plugin css for this page -->
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href=".././css/style.css">
-  <!-- endinject -->
-  <link rel="shortcut icon" href=".././images/favicon.png" />
-</head>
-  <link rel="stylesheet" href=".././vendors/iconfonts/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href=".././vendors/css/vendor.bundle.base.css">
-  <link rel="stylesheet" href=".././vendors/css/vendor.bundle.addons.css">
-<body>
-  <div class="container-scroller">
-    <!-- partial:partials/_navbar.html -->
-    <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-      <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-       
-        <h1 style="color:#0095DD; font-weight:bolder;">K.D.B</h1>
-     
-      </div>
-      <div class="navbar-menu-wrapper d-flex align-items-center">
-        <ul class="navbar-nav navbar-nav-left header-links d-none d-md-flex">
-          <li class="nav-item">
-         
-          </li>
-          <?php
- 
- //mysqli Procedural
- $conn = mysqli_connect("localhost","root","","devasom");
- if (!$conn) {
-   die("Connection failed: " . mysqli_connect_error());
- }
-  
- ?>
-         
-        </ul> <a href="./logout.php" style="color:white;">Logout</a>
-        <ul class="navbar-nav navbar-nav-right">
-          
-          <li class="nav-item dropdown d-none d-xl-inline-block">
-            <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-              <span class="profile-text"></span>
-             
-            </a>
-            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
-              <a class="dropdown-item p-0">
-                <div class="d-flex border-bottom">
-                  <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                    <i class="mdi mdi-bookmark-plus-outline mr-0 text-gray"></i>
-                  </div>
-                  <div class="py-3 px-4 d-flex align-items-center justify-content-center border-left border-right">
-                    <i class="mdi mdi-account-outline mr-0 text-gray"></i>
-                  </div>
-                  <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                    <i class="mdi mdi-alarm-check mr-0 text-gray"></i>
-                  </div>
-                </div>
-              </a>
-              <a class="dropdown-item mt-2">
-                Manage Accounts
-              </a>
-              <a class="dropdown-item">
-                Change Password
-              </a>
-              <a class="dropdown-item">
-                Check Inbox
-              </a>
-              <a class="dropdown-item">
-                Sign Out
-              </a>
-            </div>
-          </li>
-        </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-          <span class="mdi mdi-menu"></span>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Devasom Board</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="css/sl.css" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/one-page-wonder.min.css" rel="stylesheet">
+    <style>
+    .navbar-nav {
+    text-transform: uppercase;
+    background-color: black;
+    }
+    .fixed-top {
+    background-color: black;
+}
+    .btn:hover{
+      background-color: blue;
+    }
+    a{
+      color: white;
+    }
+    a:hover{
+      text-decoration: none;
+      color: white;
+    }
+    html {
+  scroll-behavior: smooth;
+}
+    </style>
+
+  </head>
+
+  <body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="./">Devasom Board</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+		    <li class="nav-item" style="color:#FFFFFF; font-weight:bolder;">
+
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="change.php">Change Password</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href=".././panel/complaint.php">File a Complaint</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href=".././panel/cart-user.php">Booking</a>
+            </li>
+            <li class="nav-item" style="color:white; margin:6px;">
+            <?php
+            $a=$_SESSION['username'];
+            echo $a;?>
+            </li>
+            <li class="nav-item">
+            <?php
+            include_once('.././login/db.php');
+              if (isset($_SESSION['username'])) : ?>
+              <a class="nav-link" href=".././login/index.php?logout='1'" style="color: red;">Log Out</a>
+              <?php endif ?> 
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-      <!-- partial:partials/_sidebar.html -->
-      <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <li class="nav-item nav-profile">
-            <div class="nav-link">
-              <div class="user-wrapper">
-               
-                <div class="text-wrapper">
-                  <p class="profile-name"></p>
-              
-                </div>
-              </div>
-           
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./users.php">
-              <i class="menu-icon mdi mdi-television"></i>
-              <span class="menu-title">Dashboard</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./complaint.php">
-              <i class="menu-icon mdi mdi-user"></i>
-              <span class="menu-title">File a Complaint</span>
-            </a>
-          </li>
-       
-            <div class="collapse" id="auth">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/samples/login.html"> Login </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/samples/register.html"> Register </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/samples/error-404.html"> 404 </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/samples/error-500.html"> 500 </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </nav>
+    <center><div class="container" style="margin-top: 200px;">
 <form method="post" action="changes.php">
 								<table>
 								<tr>
@@ -150,11 +105,11 @@
 								<tr>
 								<th>New Password</th><td><input type="password" name="np" /></td>
 								</tr>
-								<tr><td><input type="submit" value="submit" name="change"/></td>
+								<tr><td><input type="submit" class="btn btn-info" value="Submit" name="change"/></td>
 								</table>
 								
 						
-								</form></div>
+								</form></div></div>
 
    
 
