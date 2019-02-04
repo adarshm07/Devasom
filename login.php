db526e8bd81ee44e1e4659f8c973fba30f41967e
Adarsh M <adarshm954@gmail.com>
2019-02-05 00:25:44 +0530

    updated

--- login.php
+++ login.php
@@ -25,6 +25,22 @@
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+        <style>
+.flex-container {
+  display: flex;
+  justify-content: center;
+  background-color: DodgerBlue;
+}
+
+.flex-container > div {
+  background-color: #f1f1f1;
+  width: 100px;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
+}
+</style>
   </head>
   <body>
     <!-- navbar-->
@@ -56,27 +72,12 @@
       </div>
     </header>
     <section>
-    
-<!-- Col-1 -->
-        <div class="container" style="background-color: ;height: 100px; width: 140px; display: -webkit-inline-box;">
-            <div class="col-sm-4">
-              <button class="btn btn-info"><a href="./admin">Admin</a></button>
-</div>
+    <div class="flex-container">
+  <div><button class="btn btn-default"><a href="./admin">Admin</a></button></div>
+  <div><button class="btn btn-default"><a href="./manager">Manager</a></button></div>
+  <div><button class="btn btn-default"><a href="./login">User</a></button></div>  
 </div>
 
-<!-- Col-2 -->
-<div class="container" style="background-color: yellow; height: 100px; width: 140px; display: -webkit-inline-box;">
-            <div class="col-sm-4">
-            <button class="btn btn-info"><a href="./manager">Manager</a></button>
-</div>
-</div>
-
-<!-- Col-3 -->
-<div class="container" style="background-color: yellow; height: 100px; width: 140px; display: -webkit-inline-box;">
-            <div class="col-sm-4">
-            <button class="btn btn-info"><a href="./login">User</a></button>
-</div>
-</div>
 </section>
 </body>
 </html>
